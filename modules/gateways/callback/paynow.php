2ea19ef92d3723019ae6107b70ad28dd05bb5331
Jurgens Banninga <jurgens@interexcel.co.za>
2015-09-04 10:29:06 +0200

    Fix invoice date

--- modules/gateways/callback/paynow.php
+++ modules/gateways/callback/paynow.php
@@ -84,7 +84,7 @@ if ($status=="true") {
     $values["amount"] = $amount;
     $values["fee"] = $fee;
     $values["gateway"] = $GATEWAY['name'];
-    $values["date"] = $GATEWAY['name'];
+    $values["date"] = date('Ymd');
     $results = localAPI($command,$values,$adminuser);
 
     pnLog( 'addinvoicepayment Result: '. print_r( $results, true ) );
