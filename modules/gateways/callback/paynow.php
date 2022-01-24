96e3bd07d6db43054d4dcbfb4c52de7cbf22d0d1
Jurgens <jurgens@iedm.co.za>
2022-01-24 15:25:25 +0200

    Fix redirect failing sometimes due to "duplicate" transaction

--- modules/gateways/callback/paynow.php
+++ modules/gateways/callback/paynow.php
@@ -7,7 +7,7 @@ include("../../../includes/gatewayfunctions.php");
 include("../../../includes/invoicefunctions.php");
 
 # Set to true to enable logging
-define('PN_DEBUG', false);
+define('PN_DEBUG', true);
 
 /**
  * pnlog
@@ -60,18 +60,92 @@ preg_match('/(\d{1,8})-/', $_POST["Reference"], $matches);
 
 $invoiceid = $matches[1];
 $transid = $_POST["Reference"];
-$amount = $_POST["Amount"];
+$amountZAR = $_POST["Amount"];
+$convertedToCurrencyID = $_POST["Extra2"]; // ZAR
+$convertedFromCurrencyID = $_POST["Extra3"]; // USD, GBP, etc
+
 $fee = "";
 $adminuser = $GATEWAY['whmcs_admin_username'];
 
+if(!$invoiceid) {?>
+	<script type="text/javascript">
+        setTimeout(function () {
+           window.location.href = "../../../clientarea.php";
+        }, 5000);
+    </script>
+    <?php
+    pnLog( 'Ivalid invoice id. Returning to clientarea.' );
+}
+
 $invoiceid = checkCbInvoiceID($invoiceid,$GATEWAY["name"]); # Checks invoice ID is a valid invoice number or ends processing
 
-pnLog( 'Invoice Id: '. print_r( $invoiceid, true ));
+pnLog( 'Invoice Id: '. print_r( $invoiceid, true ) );
+pnLog( 'Transaction Id: '. print_r( $transid, true ) );
 pnLog( 'Status: '. print_r( $status, true ) . ' - ' .  gettype($status) );
+
+function redirectToSuccess() {
+    echo "<p>Payment was successful.</p>";
+    echo "<p>You will be redirected to the client area in 5 seconds. <a href='../../../clientarea.php'>Click here</a> to return immediately.</p>";
+    ?>
+
+    <script type="text/javascript">
+        setTimeout(function () {
+           window.location.href = "../../../clientarea.php";
+        }, 5000);
+    </script>
+    <?php
+}
+
+/**
+ * Check whether a transaction id already exists
+ * 
+ * @param $invoiceid The invoice to check
+ * @param $transid The transaction id to check
+ * 
+ * @param bool True if the transaction id DOES NOT exist. False if it DOES. 
+ */
+function isTransactionIdUnique($invoiceid, $transid) {
+    $command = 'GetTransactions';
+    $postData = array(
+        'invoiceid' => $invoiceid,
+    );
+    // $adminUsername = 'ADMIN_USERNAME'; // Optional for WHMCS 7.2 and later
+
+    $results = localAPI($command, $postData, $adminUsername);
+    
+    $transactionids = [];
+
+    if($results && isset($results['result']) && $results['result'] === 'success') {
+        if($results['totalresults'] > 0) {
+            // We got some transactions
+            foreach($results['transactions']['transaction'] as $t) {
+                $transactionids[] = $t['transid'];
+            }
+        }
+    }
+
+    if(in_array($transid, $transactionids)) {
+        // Transaction id exists
+        return false;
+    }
+
+    // Couldn't find the transaction ID
+    return true;
+}
 
 if ($status=="true") {
     # Successful
     pnLog( 'Transaction Successful' );
+
+    # Check if transaction ID exists. If so, redirect to clientarea.
+    # This is needed because the gateway calls the same script twice (via notify and via success) and 
+    # the checkCbTransID() will die() and show a blank screen
+    if(!isTransactionIdUnique($invoiceid, $transid)) {
+        // Redirect
+        pnLog( "Skipping checkCbTransID call... Transaction '{$transid}' exists" );
+        redirectToSuccess();
+        die();
+    }
 
     checkCbTransID($transid); # Checks transaction number isn't already in the database and ends processing if it does
     pnLog( 'checkCbTransID Called' );
@@ -91,21 +165,13 @@ if ($status=="true") {
 
     logTransaction($GATEWAY["name"],$_POST,"Successful"); # Save to Gateway Log: name, data array, status
 
-    echo "<p>Payment was successful.</p>";
-    echo "<p>You will be redirected to the client area in 5 seconds. <a href='../../../clientarea.php'>Click here</a> to return immediately.</p>";
-    ?>
-
-    <script type="text/javascript">
-        setTimeout(function () {
-           window.location.href = "../../../clientarea.php";
-        }, 5000);
-    </script>
-    <?php
+    redirectToSuccess();
     pnLog( 'Returning to clientarea.' );
 } else {
     # Unsuccessful
     pnLog( 'Transaction Unsuccessful' );
     logTransaction($GATEWAY["name"],$_POST,"Unsuccessful"); # Save to Gateway Log: name, data array, status
+
     echo "<p>Payment was declined. Reason: " . $_POST['Reason'] . "</p>";
     echo "<p><a href='../../../cart.php'>Click here</a> to return to the cart.</p>";
 }
